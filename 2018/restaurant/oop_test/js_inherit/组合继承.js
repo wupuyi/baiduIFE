b25fbbc4496f2797bfe5714f9c267d741041f523
wupuyi <wupuyi@foxmial.com>
2018-08-07 18:11:53 +0800

    继承

---
+++ 2018/restaurant/oop_test/js_inherit/组合继承.js
@@ -0,0 +1,23 @@
+// 也叫经典继承，将原型链和借用构造函数的技术组合到一起
+function SuperType(name) {
+  this.name = name;
+  this.colors = ['red', 'blue', 'green'];
+}
+
+SuperType.prototype.sayName = function () {
+  console.log(this.name);
+};
+
+function SubType(name, age) {
+  // 继承属性
+  SuperType.call(this, name);
+
+  this.age = age;
+}
+
+// 继承方法
+SubType.prototype = new SuperType();
+SubType.prototype.constructor = SubType;
+SubType.prototype.sayAge = function () {
+  console.log(this.age);
+}
