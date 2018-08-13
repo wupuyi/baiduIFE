0d2ff57330baf9f353e93a5785884fa13d024cb2
wupuyi <wupuyi@foxmial.com>
2018-08-13 15:21:06 +0800

    函数递归

---
+++ 2018/restaurant/oop_test/factorial.js
@@ -0,0 +1,11 @@
+var factorial = (function f (num) {
+  if (num <= 1) {
+    return 1;
+  } else {
+    return num * f(num - 1);
+  }
+});
+
+var anotherFactorial = factorial;
+factorial = null;
+console.log(anotherFactorial(4))
